0d6fcafd6a001d96e8766793d5010497b09e5e53
Michael Kappert <michael.kappert@gmx.net>
2024-02-03 19:33:14 +0100

    Fix sail names.. again

--- web/js/router-rs.js
+++ web/js/router-rs.js
@@ -390,7 +390,7 @@ import * as Router from './router.js';
         Router.setUp(getVMG);
         Router.settings.presets = "RS";
         Router.settings.options = ["realsail"];
-        Router.setSailnames(["Jib", "Spi", "Gennaker"]);
+        Router.setSailnames(["Jib", "Gennaker", "Spi"]);
         
         document.getElementById("bt_nmeaupdate").addEventListener("click", getBoatPosition);
         document.getElementById("bt_nmeareset").addEventListener("click", resetNMEAConnection);
